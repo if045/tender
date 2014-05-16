b5fef9570c7fb330048e213bb71adeb1d87d6c91
Sergiy2014 <sergiy.vandzilyak@gmail.com>
2014-05-16 12:38:07 +0300

    data for all tables

---
+++ src/main/resources/sql/demoData/data_for_all_tables.sql
@@ -0,0 +1,262 @@
+USE tender;
+
+INSERT INTO checked_status (name) VALUES
+('Open'),
+('In progress'),
+('Checked'),
+('Denied');
+
+
+INSERT INTO tender_status (name) VALUES
+('Open'),
+('In progress'),
+('Close');
+
+
+INSERT INTO conflict_status (name) VALUE
+('Open'),
+('In progress'),
+('resolved');
+
+
+INSERT INTO role (name) VALUES
+('admin'),
+('moderator'),
+('seller'),
+('customer');
+
+
+INSERT INTO measurement (name) VALUES
+('Kg'),
+('Km'),
+('Liter'),
+('Box'),
+('Unit'),
+('Ton'),
+('Sm'),
+('Humen/Hour');
+
+
+INSERT INTO address (city, street, building_number, postcode) VALUES 
+('Vilnohirsk', 'Stepova', 1, 27500),
+('Drohobych', 'Khmelnitsky', 5, 29000),
+('Kiev', 'Moskovska', 21, 01015),
+('Nizhyn', 'Vosstaniya', 8, 16500),
+('Dnipropetrovsk', 'Yuzhnaya', 12, 22300),
+('Donetsk', 'Dzerzhinsky', 14, 12500),
+('Kiev', 'Solomenskaya', 21, 26600),
+('Odessa', 'Bunin', 32, 24500),
+('Simferopol', 'Timoshenko', 55, 23300),
+('Kiev', 'Narodnogo Opolcheniy', 7, 07500),
+('Kiev', 'Karla Marksa', 9, 12500),
+('Alchevsk', 'Moskovskaya', 34, 32500),
+('Novoukrayinka', 'Pershoi Mayivki', 55, 21500),
+('Lviv', 'Kravchenko', 2, 26600),
+('Odessa', 'Bugaevskaya', 3, 25600);
+
+
+INSERT INTO company (name, srn, email, logo, address_id) VALUES 
+('Vilnohirsk Mining Metallurgical Plant', 121345, 'Vilnohirsk@gmail.com', null, 1),
+('Podolsky Smak', 122345, 'PodolskySmak@gmail.com', null, 2),
+('Econom-servis Gp CffInfo', 123345, 'EconomservisGp@gmail.com', null, 3),
+('Epcf Syaivo LtdInfo', 124345, 'EpcfSyaivo@gmail.com', null, 4),
+('Planet Service', 125345, 'PlanetService@gmail.com', null, 5),
+('Global Titan Distribution Inc', 126345, 'GlobalTitan@gmail.com', null, 6),
+('Sea Electronics', 127345, 'SeaElectronics@gmail.com', null, 7),
+('Kaalbye Logistics Ltd', 128345, 'KaalbyeLogistics@gmail.com', null, 8),
+('Energy Xxii', 129345, 'EnergyXxii@gmail.com', null, 9),
+('Eurocom Components', 120345, 'EurocomComponents@gmail.com', null, 10),
+('Natural Foods', 102345, 'NaturalFoods@gmail.com', null, 11),
+('Cory Sro', 112345, 'CorySro@gmail.com', null, 12),
+('Polymercontainer Plant', 122345, 'PolymerPlant@gmail.com', null, 13),
+('Xado Corp', 132345, 'XadoCorp@gmail.com', null, 14),
+('Garbuz Studio', 142345, 'GarbuzStudio@gmail.com', null, 15);
+
+
+INSERT INTO user (login, password, create_date) VALUES
+('odin_ogame@ukr.net','arlekin',CURDATE()),
+('mail4quest@mail.ru','quest',CURDATE()),
+('vasapupkin@gmail.com','pupkin',CURDATE()),
+('vasahopnik@rambler.ru','dver88',CURDATE()),
+('dota2@gmail.com','noob23',CURDATE()),
+('olgamoros@ukr.net','ole4ka',CURDATE()),
+('sanja_ivanow@ukr.net','123qwerty',CURDATE()),
+('kolja234@qmail.com','vasa567',CURDATE());
+
+
+INSERT INTO profile (icon, first_name, last_name, birthday, telephone, checked, person, user_id, company_id) VALUES 
+(null, 'Ivan', 'Nesamay', '1986-05-28', '55-559-5', false, 'L', 1, 1),
+(null, 'Vasja', 'Pupkin', '1966-02-18', '55-559-5', false, 'L', 2, 2),
+(null, 'Kolja', 'Qwerty', '1996-01-02', '55-559-5', false, 'L', 3, 3),
+(null, 'Olja', 'Semenova', '1984-07-09', '55-559-5', false, 'L', 4, 4),
+(null, 'Katja', 'Moros', '1985-07-08', '55-559-5', false, 'L', 5, 5),
+(null, 'Nasar', 'Havruliv', '1976-06-08', '55-559-5', false, 'L', 6, 6),
+(null, 'Vasul', 'Sobol', '1989-02-02', '55-559-5', false, 'L', 7, 7),
+(null, 'Oksana', 'Sobol', '1983-08-01', '55-559-5', false, 'L', 8, 8);
+
+
+INSERT INTO location (name) VALUES 
+('Autonomous Republic of Crimea'),
+('Cherkasy Region'),
+('Chernihiv Region'), 
+('Chernivtski Region'), 
+('Dnipropetrovsk Region'), 
+('Donetsk Region'),
+('Ivano-Frankivsk Region'),
+('Kharkiv Region'),
+('Kherson Region'), 
+('Khmelnytsky Region'),
+('Kirovograd Region'),
+('Kyiv Region'),
+('Luhansk Region'),
+('Lviv Region'),
+('Mykolayiv Region'),
+('Odesa Region'),
+('Poltava Region'),
+('Rivne Region'),
+('Sumy Region'),
+('Ternopil Region'),
+('Vinnytsya Region'),
+('Volyn region'),
+('Zakarpattya Region'),
+('Zaporizhzhya Region'),
+('Zhytomyr Region');
+
+
+INSERT INTO deal_status (name) VALUES 
+('open'),
+('in progress'),
+('close');
+
+
+INSERT INTO category (name, parent) VALUES
+('Building', NULL),
+('Animals', NULL),
+('Pet', 2),
+('Exotic', 2),
+('Dogs', 3),
+('Birds', 3),
+('Product', NULL),
+('Meat', 7),
+('Fish', 7),
+('Milk', 7);
+
+
+INSERT INTO tender (author_id, title, create_date, end_date, status_id, suitable_price, description) VALUES
+(1, 'Meal', CURDATE(), CURDATE(), 1, 1000000.00, NULL),
+(2, 'Materials', CURDATE(), CURDATE(), 2, 500000.00, NULL),
+(3, 'Haus Building', CURDATE(), CURDATE(), 3, 1600000.00, NULL),
+(4, 'Cars', CURDATE(), CURDATE(), 2, 9900000.00, NULL),
+(5, 'Animals', CURDATE(), CURDATE(), 1, 19000000.00, NULL),
+(6, 'Tourism', CURDATE(), CURDATE(), 3, 23000000.00, NULL),
+(7, 'Industry', CURDATE(), CURDATE(), 1, 450000.00, NULL),
+(8, 'Products', CURDATE(), CURDATE(), 1, 113000000.00, NULL),
+(4, 'Building Service', CURDATE(), CURDATE(), 2, 14000000.00, NULL),
+(2, 'Furniture', CURDATE(), CURDATE(), 2, 76000000.00, NULL),
+(3, 'All in 0ne', CURDATE(), CURDATE(), 2, 59000000.00, NULL);
+
+
+INSERT INTO proposal (discount_percentage, discount_currency, description, seller_id, tender_id) VALUES
+(10, null, 'If you wil take all', 1, 10),
+(20, null, null, 1, 9),
+(30, null, 'and free delivery', 2, 8),
+(50, null, null, 3, 7),
+(null, 10000.00, null, 4, 6),
+(null, 5000.00, null, 5, 5),
+(null, 8000.00, null, 6, 4),
+(null, 1400.00, null, 7, 3),
+(null, 1500.00, null, 8, 2),
+(null, 500.00, null, 4, 1),
+(null, 5000.00, null, 1, 7);
+
+
+INSERT INTO item (name, type, category_id) VALUES
+('Rocks', 'P', 1),
+('Dogs', 'P', 2),
+('Cats', 'P', 3),
+('Puffin', 'P', 4),
+('Ovcharka', 'P', 5),
+('Penguins', 'S', 6),
+('Juises', 'S', 7),
+('Salami', 'S', 8),
+('Gold fish', 'S', 9),
+('jogurt', 'S', 10),
+('Dolmantin', 'S', 5);
+
+
+INSERT INTO unit (quantity, measurement_id, item_id) VALUES
+(500, 1, 10),
+(100, 2, 9),
+(5300, 3, 8),
+(1200, 4, 7),
+(2300, 5, 6),
+(1000, 6, 5),
+(666, 3, 4),
+(999, 1, 3),
+(1023, 4, 2),
+(512, 3, 1);
+
+
+INSERT INTO checked_profile (profile_id, moderator_id, status_id) VALUES
+(1,1,1),
+(2,2,2),
+(3,2,1),
+(4,1,3),
+(5,3,3),
+(6,1,4),
+(7,2,3),
+(8,2,3),
+(3,2,2);
+
+
+INSERT INTO bid (price, date, proposal_id, unit_id) VALUES
+(1000.00, CURDATE(), 1, 1),
+(60000.00, CURDATE(), 2, 2),
+(1200.00, CURDATE(), 3, 3),
+(56000.00, CURDATE(), 4, 4),
+(56000.00, CURDATE(), 5, 5),
+(56000.00, CURDATE(), 6, 6),
+(56000.00, CURDATE(), 7, 7),
+(56000.00, CURDATE(), 8, 8),
+(56000.00, CURDATE(), 9, 9),
+(56000.00, CURDATE(), 10, 10);
+
+
+INSERT INTO conflict (moderator_id, bid_id, description, status_id) VALUE
+(1,1,'text1',1),
+(2,2,'text2',2),
+(3,3,'text3',2),
+(2,4,'text4',2),
+(2,5,'text5',3),
+(1,6,'text6',1),
+(1,7,'text7',1),
+(3,8,'text8',3),
+(2,9,'text9',1);
+
+
+INSERT INTO comment (tender_id, user_id, message, date) VALUES 
+(1, 1, 'This price is too high', CURDATE()), 
+(1, 2, 'This is my opinion', CURDATE()),
+(2, 1, 'hello', CURDATE()),
+(2, 1, 'This price is too high', CURDATE()),
+(2, 3, 'Some text', '2014-07-10'), 
+(1, 1, 'You can buy it much cheaper', CURDATE()),
+(1, 2, 'Ok, i will think about it.', CURDATE()), 
+(2, 1, 'This is too low price.', CURDATE()), 
+(2, 2, 'I do not think so.', CURDATE());
+
+
+INSERT INTO deal (bid_id, proposal_id, customer_id, seller_id, sum, date, status_id) VALUES 
+(1, 1, 1, 1, 100000.00, CURDATE(), 1),
+(3, 1, 2, 1, 55000.00, CURDATE(), 1),
+(2, 3, 5, 2, 40000.00, CURDATE(), 1),
+(5, 4, 4, 5, 400000.00, CURDATE(), 2),
+(10, 6, 2, 6, 25000.00, CURDATE(), 2),
+(4, 1, 5, 3, 1000.00, CURDATE(), 2),
+(4, 8, 4, 7, 240000.00, CURDATE(), 1),
+(1, 4, 5, 2, 1250500.00, CURDATE(), 1),
+(9, 9, 6, 3, 56000.00, CURDATE(), 3),
+(6, 6, 6, 7, 95600.00, CURDATE(), 3);
+
+
+
