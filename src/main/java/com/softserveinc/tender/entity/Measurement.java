de4344633a6d5dfa22e7e0013201cc071bb6f4ff
GUSb <GUSbYa@@gmail.com>
2014-05-19 10:01:12 +0300

    fixed mapping in classes Deal, DealStatus, Measurement

--- src/main/java/com/softserveinc/tender/entity/Measurement.java
+++ src/main/java/com/softserveinc/tender/entity/Measurement.java
@@ -1,18 +1,20 @@
 package com.softserveinc.tender.entity;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "measurement")
 public class Measurement {
+
+    private static final int MEASUREMENT_NAME_LENGTH = 15;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Integer id;
 
-    @Column(name = "name")
+    @Column(name = "name", unique = true, nullable = false, length = MEASUREMENT_NAME_LENGTH)
     private String name;
 
     public Integer getId() {
