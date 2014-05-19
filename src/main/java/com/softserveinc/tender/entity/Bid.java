448faa53fce7e37e8a3794cf1530977f873ac4e6
Sergiy2014 <sergiy.vandzilyak@gmail.com>
2014-05-19 12:29:34 +0300

    Repo, Hibernate/ JPA mapping for table bid

--- src/main/java/com/softserveinc/tender/entity/Bid.java
+++ src/main/java/com/softserveinc/tender/entity/Bid.java
@@ -1,11 +1,73 @@
 package com.softserveinc.tender.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
+@Entity
+@Table(name = "bid")
 public class Bid {
+
+    private static final int MINIMUM_SUM_VALUE = 0;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "price", nullable = false)
+    @Size(min = MINIMUM_SUM_VALUE)
     private Double price;
+
+    @Column(name = "date", nullable = false)
     private Date date;
+
+    @OneToMany
+    @JoinColumn(name = "unit_id")
     private Unit unit;
+
+
+    @OneToMany
+    @JoinColumn(name = "proposal_id")
     private Proposal proposal;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+
+    public Proposal getProposal() {
+        return proposal;
+    }
+
+    public void setProposal(Proposal proposal) {
+        this.proposal = proposal;
+    }
 }
---
+++ src/main/java/com/softserveinc/tender/service/BidService.java
@@ -0,0 +1,11 @@
+package com.softserveinc.tender.service;
+
+
+import com.softserveinc.tender.entity.Bid;
+
+import java.util.List;
+
+public interface BidService {
+    List<Bid> findAll();
+    Bid findById(int id);
+}
