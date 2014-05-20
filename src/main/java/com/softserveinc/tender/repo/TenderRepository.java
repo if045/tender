bd489448ba394573cfdee80d8d0ef02d0b6946ff
KhrystynaMakar <tinochka0@gmail.com>
2014-05-20 11:24:39 +0300

    change method name for filter

--- src/main/java/com/softserveinc/tender/repo/TenderRepository.java
+++ src/main/java/com/softserveinc/tender/repo/TenderRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public interface TenderRepository extends JpaRepository<Tender, Integer> {
     @Query("select t from Tender t where t.suitablePrice between :min and :max")
-    List<Tender> filter(@Param("min") double min,
-                        @Param("max") double max);
+    List<Tender> findBySuitablePrice(@Param("min") double min,
+                                     @Param("max") double max);
 
 }
