07e702be5133e48ad7a9ac893ddd9c099d2bd1cf
KhrystynaMakar <tinochka0@gmail.com>
2014-05-20 12:24:57 +0300

    Fixed proposal bugs

--- src/main/java/com/softserveinc/tender/service/ProposalService.java
+++ src/main/java/com/softserveinc/tender/service/ProposalService.java
@@ -5,8 +5,8 @@ import com.softserveinc.tender.entity.Proposal;
 import java.util.List;
 
 public interface ProposalService {
-    List<Proposal> findByTender();
-    List<Proposal> findBySeller();
+    List<Proposal> findByTender(Integer tenderId);
+    List<Proposal> findBySeller(Integer sellerId);
     Proposal findById(Integer id);
     void save(Proposal proposal);
     void deleteById(Integer id);
