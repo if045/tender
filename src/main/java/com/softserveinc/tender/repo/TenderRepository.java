afcc15a75fcc9de255706d66e3ac6fcabfe302c8
dreadnough <odin_ogame@ukr.net>
2014-05-21 13:11:26 +0300

    fix automatic merging with develop(tender.jsp)

--- src/main/java/com/softserveinc/tender/repo/TenderRepository.java
+++ src/main/java/com/softserveinc/tender/repo/TenderRepository.java
@@ -10,8 +10,5 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface TenderRepository extends JpaRepository<Tender,Integer> {
-   // @Query("select distinct location from Tender tender inner join tender.locations location ")
-  //  List<Location> tendersLocation();
-   // @Query("SELECT b FROM Tender b where b.location.id = :id")
-//   public List<Location> findByLocation(@Param("id") int id);
+
 }
