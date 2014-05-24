ce8ceb79bcc5152d5ab00ac36e5bca92d2bae18d
Rostyslav Chaban <Rostyslav.Chaban@medhost.com>
2014-05-24 14:44:28 +0300

    Merged with develop

--- src/main/java/com/softserveinc/tender/service/impl/TenderServiceImpl.java
+++ src/main/java/com/softserveinc/tender/service/impl/TenderServiceImpl.java
@@ -1,18 +1,12 @@
 package com.softserveinc.tender.service.impl;
 
-import com.softserveinc.tender.entity.Location;
 import com.softserveinc.tender.entity.Tender;
-import com.softserveinc.tender.entity.Unit;
 import com.softserveinc.tender.repo.TenderRepository;
 import com.softserveinc.tender.repo.UnitRepository;
 import com.softserveinc.tender.service.TenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sun.management.counter.Units;
-
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -45,8 +39,12 @@ public class TenderServiceImpl implements TenderService {
         return tender;
     }
 
+    public List<Tender> findByCustomParameters(Double min, Double max, List<Integer> status, List<Integer> categories, List<Integer> locations) {
+        return tenderRepository.findByCustomParameters(min, max, status, categories, locations);
+    }
+
     @Override
-    public List<Tender> findByCustomParameters(Double min, Double max, Integer[] status,List<Integer> locations) {
-        return tenderRepository.findByCustomParameters(min, max, status,locations);
+    public List<Tender> findByCategory(List<Integer> categories) {
+        return tenderRepository.findByCategory(categories);
     }
 }
--- src/main/resources/sql/test.sql
+++
@@ -1,3 +0,0 @@
-some other text
-some text
-
