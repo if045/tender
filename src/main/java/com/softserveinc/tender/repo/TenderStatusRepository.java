ae2917540a43936e8e2d6831920031945ca74e7e
GUSb <GUSbYa@gmail.com>
2014-05-30 09:45:12 +0300

    statuses order by id

--- src/main/java/com/softserveinc/tender/repo/TenderStatusRepository.java
+++ src/main/java/com/softserveinc/tender/repo/TenderStatusRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface TenderStatusRepository extends JpaRepository<TenderStatus, Integer> {
 
-    @Query("select distinct s from Tender t inner join t.status s")
+    @Query("select distinct s from Tender t inner join t.status s order by s.id asc")
     List<TenderStatus> getAllTenderStatuses();
 }
