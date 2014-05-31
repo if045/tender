8b5a800c7b16077e89149bfdb0480782502236bc
GUSb <GUSbYa@gmail.com>
2014-05-31 23:02:55 +0300

    added query to proposal reposirory

--- src/main/java/com/softserveinc/tender/repo/ProposalRepository.java
+++ src/main/java/com/softserveinc/tender/repo/ProposalRepository.java
@@ -2,9 +2,15 @@ package com.softserveinc.tender.repo;
 
 import com.softserveinc.tender.entity.Proposal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface ProposalRepository extends JpaRepository<Proposal, Integer> {
-    List<Proposal> findByTender(Integer tenderId);
+
+    @Query("select p from Proposal p where p.tender.id = :tenderId")
+    List<Proposal> findByTender(@Param("tenderId") Integer tenderId);
+
     List<Proposal> findBySeller(Integer sellerId);
 }
