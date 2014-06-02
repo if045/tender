af797e5268be80219cc38a2bb42a68347019d0e9
yuramitsyuk <ptytb@yandex.ru>
2014-06-02 17:08:34 +0300

    16 and 17 items from bug list (add hibernate properties)

--- src/main/java/com/softserveinc/tender/service/impl/TenderServiceImpl.java
+++ src/main/java/com/softserveinc/tender/service/impl/TenderServiceImpl.java
@@ -6,6 +6,7 @@ import com.softserveinc.tender.repo.TenderRepository;
 import com.softserveinc.tender.service.TenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -20,6 +21,12 @@ public class TenderServiceImpl implements TenderService {
 
     @Override
     public List<Tender> findByCustomParameters(TenderFilter tenderFilter) {
-        return tenderRepository.findByCustomParameters(tenderFilter.getMinPrice(), tenderFilter.getMaxPrice(), tenderFilter.getStatuses(), tenderFilter.getCategories(), tenderFilter.getLocations(), tenderFilter.getItems(), tenderFilter.getMinDate(), tenderFilter.getMaxDate(), tenderFilter.getCategoryFlag(), tenderFilter.getItemFlag(), tenderFilter.getLocationFlag(), tenderFilter.getStatusFlag(),tenderFilter.getPriceFlag());
+        return tenderRepository.findByCustomParameters(tenderFilter.getMinPrice(), tenderFilter.getMaxPrice(),
+                                                       tenderFilter.getStatuses(), tenderFilter.getCategories(),
+                                                       tenderFilter.getLocations(), tenderFilter.getItems(),
+                                                       tenderFilter.getMinDate(), tenderFilter.getMaxDate(),
+                                                       tenderFilter.getCategoryFlag(), tenderFilter.getItemFlag(),
+                                                       tenderFilter.getLocationFlag(), tenderFilter.getStatusFlag(),
+                                                       tenderFilter.getPriceFlag());
     }
 }
