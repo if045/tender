a09e1f4fa52b76d54df52ce08291f99e922a2f49
GUSb <GUSbYa@gmail.com>
2014-06-02 13:03:39 +0300

    made query method and service logic

--- src/main/java/com/softserveinc/tender/service/impl/TenderStatusServiceImpl.java
+++ src/main/java/com/softserveinc/tender/service/impl/TenderStatusServiceImpl.java
@@ -32,4 +32,9 @@ public class TenderStatusServiceImpl implements TenderStatusService{
     public List<TenderStatus> getAllTenderStatuses() {
         return tenderStatusRepository.getAllTenderStatuses();
     }
+
+    @Override
+    public List<TenderStatus> findActiveTendersStatuses() {
+        return tenderStatusRepository.findByActive(true);
+    }
 }
