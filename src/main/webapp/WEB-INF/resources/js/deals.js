97662280883ff708d3816e3fd52b8693445e76cc
rsolov <romansoloviy@gmail.com>
2014-06-04 00:02:59 +0300

    Deleted unused code.

--- src/main/webapp/WEB-INF/resources/js/deals.js
+++ src/main/webapp/WEB-INF/resources/js/deals.js
@@ -2,15 +2,7 @@ $(document).ready(function() {
     $('#endDate').datepicker({
         format: 'mm-dd-yyyy',
          startDate: '-5y'
-    });
-
-    $('#search_input').keydown(function(event) {
-        if (event.keyCode == 13) {
-            //search query
-
-            return false;
-        }
-    });   
+    });  
 });
 
 function showDeals() {
