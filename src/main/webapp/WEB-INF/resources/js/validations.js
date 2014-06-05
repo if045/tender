7d0b1a04c5b59886edf4d4753f89228e429d67e6
Sergiy2014 <sergiy.vandzilyak@gmail.com>
2014-06-05 17:28:16 +0300

    fix validatoin

--- src/main/webapp/WEB-INF/resources/js/validations.js
+++ src/main/webapp/WEB-INF/resources/js/validations.js
@@ -1,6 +1,89 @@
 $(document).ready(function () {
 
-    $("#create_tender_form_validation").validate({
+    var validationForTenderHomePrice = {
+        rules: {
+
+            price: {
+                number: true
+
+            }
+        },
+
+        showErrors: function(errorMap, errorList) {
+            if(errorList.length) {
+                $("div.error").html(errorList[0].message);
+            }else{
+                $("div.error").html("");
+            }
+        }
+    };
+
+    var validationForAllInput = {
+        rules: {
+            title: {
+                required: true,
+                minlength: 4,
+                maxlength: 30
+            },
+
+            price: {
+                required: true,
+                number: true
+
+            },
+
+            quantity: {
+                required: true,
+                digits: true
+            },
+
+            allowance: {
+                required: true,
+                number: true,
+                minlength: 1,
+                maxlength: 3
+            },
+
+            description: {
+                maxlength: 100
+            }
+        },
+        messages: {
+            title: {
+                required: "This field can not be empty!",
+                minlength: "The minimum length of 4 character",
+                maxlength: "The maximum length of 30 characters"
+            },
+
+            price: {
+                required: "This field can not be empty!",
+                number: "Input format - numbers!"
+            },
+
+            quantity: {
+                required: "This field can not be empty!",
+                digits: "Input format - digits!"
+            },
+
+            allowance: {
+                required: "This field can not be empty",
+                number: "Input format - numbers",
+                minlength: "The minimum length of 1 character",
+                maxlength: "The maximum length of 3 characters"
+            },
+
+            description: {
+                maxlength: "The maximum length of 100 characters"
+            }
+        }
+    };
+
+    jQuery("#create_tender_form_validation").validate(validationForAllInput);
+    jQuery("#edit_tender_form").validate(validationForAllInput);
+    jQuery("#create_proposal_form").validate(validationForAllInput);
+    jQuery("#tendersHome_validation").validate(validationForTenderHomePrice);
+
+    /*$("#create_tender_form_validation").validate({
 
         rules: {
 
@@ -112,5 +195,5 @@ $(document).ready(function () {
                 description: "The maximum length of 100 characters"
             }
         });
-    }
+    }*/
 });
