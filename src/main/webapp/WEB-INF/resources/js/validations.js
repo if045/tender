d1f7d0a5e2633e16a687264f30a30c5a6cc394f0
Sergiy2014 <sergiy.vandzilyak@gmail.com>
2014-06-05 17:30:00 +0300

    fix validatoin

--- src/main/webapp/WEB-INF/resources/js/validations.js
+++ src/main/webapp/WEB-INF/resources/js/validations.js
@@ -2,17 +2,15 @@ $(document).ready(function () {
 
     var validationForTenderHomePrice = {
         rules: {
-
             price: {
                 number: true
-
             }
         },
 
-        showErrors: function(errorMap, errorList) {
-            if(errorList.length) {
+        showErrors: function (errorMap, errorList) {
+            if (errorList.length) {
                 $("div.error").html(errorList[0].message);
-            }else{
+            } else {
                 $("div.error").html("");
             }
         }
@@ -29,7 +27,6 @@ $(document).ready(function () {
             price: {
                 required: true,
                 number: true
-
             },
 
             quantity: {
@@ -48,6 +45,7 @@ $(document).ready(function () {
                 maxlength: 100
             }
         },
+
         messages: {
             title: {
                 required: "This field can not be empty!",
@@ -82,118 +80,4 @@ $(document).ready(function () {
     jQuery("#edit_tender_form").validate(validationForAllInput);
     jQuery("#create_proposal_form").validate(validationForAllInput);
     jQuery("#tendersHome_validation").validate(validationForTenderHomePrice);
-
-    /*$("#create_tender_form_validation").validate({
-
-        rules: {
-
-            title: {
-                required: true,
-                minlength: 4,
-                maxlength: 30
-            },
-
-            price: {
-                required: true,
-                number: true
-
-            },
-
-            quantity: {
-                required: true,
-                digits: true
-            },
-
-            description: {
-                maxlength: 100
-            }
-        },
-
-        messages: {
-
-            title: {
-                required: "This field can not be empty!",
-                minlength: "The minimum length of 4 character",
-                maxlength: "The maximum length of 30 characters"
-            },
-
-            price: {
-                required: "This field can not be empty!",
-                number: "Input format - numbers!"
-            },
-
-            quantity: {
-                required: "This field can not be empty!",
-                digits: "Input format - digits!"
-            },
-
-            description: {
-                maxlength: "The maximum length of 100 characters"
-            }
-        }
-    });
-
-    $("#edit_tender_form").validate({
-       rules: {
-           description: {
-               maxlength: 100
-           }
-       },
-
-        messages: {
-            description: {
-                maxlength: "100 characters - the maximum length you can enter"
-            }
-        }
-    });
-
-    $('#create_proposal_form').validate({
-        rules: {
-            description: {
-                maxlength: 100
-            },
-
-            allowance: {
-                required: true,
-                number: true,
-                minlength: 1,
-                maxlength: 3
-            },
-
-            price: {
-                number: true
-            }
-        },
-
-        messages: {
-            allowance: {
-                required: "This field can not be empty",
-                number: "Input format - numbers",
-                minlength: "The minimum length of 1 character",
-                maxlength: "The maximum length of 3 characters"
-            },
-
-            description: {
-                maxlength: "100 characters - the maximum length you can enter"
-            },
-
-            price: {
-                number: "Input format - numbers"
-            }
-        }
-    });
-
-    function validateDescription(id) {
-        $('#' + id).validate({
-           rules: {
-               description: {
-                   maxlength: 100
-               }
-           } ,
-
-            messages: {
-                description: "The maximum length of 100 characters"
-            }
-        });
-    }*/
 });
