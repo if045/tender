dd42453be56043e9e5a52730f43174d252d01e60
dreadnough <odin_ogame@ukr.net>
2014-06-05 09:04:40 +0300

    fixed bugs with empty field

--- src/main/webapp/WEB-INF/resources/js/tender.js
+++ src/main/webapp/WEB-INF/resources/js/tender.js
@@ -17,7 +17,7 @@ function showProposals() {
         var html;
         var len = data.length;
         if(len > 0) {
-            document.getElementById("no_proposals_message").setAttribute('hidden', 'true');
+            document.getElementById("no_proposals_message").setAttribute('hidden','true');
             for (var i = 0; i < len; i++) {
                 html += '<tr>' +
                     '<td align="center">' + data[i].fullName + '</td>' +
