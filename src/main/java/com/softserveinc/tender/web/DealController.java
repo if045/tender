cc47afbc276bede7c44a97f36ad36b88b952a4af
dreadnough <odin_ogame@ukr.net>
2014-06-11 12:09:21 +0300

    resolved conflict

--- src/main/java/com/softserveinc/tender/web/DealController.java
+++ src/main/java/com/softserveinc/tender/web/DealController.java
@@ -1,18 +1,14 @@
 package com.softserveinc.tender.web;
 
 import com.softserveinc.tender.dto.DealDto;
-import com.softserveinc.tender.dto.FeedbackSaveDto;
 import com.softserveinc.tender.facade.DealServiceFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -26,7 +22,7 @@ public class DealController {
     private DealServiceFacade dealFacade;
 
     @RequestMapping(method = RequestMethod.GET)
-      public @ResponseBody List<DealDto> findDeals() {
+    public @ResponseBody List<DealDto> findDeals() {
         return dealFacade.findAllDeals();
     }
 
