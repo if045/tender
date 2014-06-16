ffc48e28b0a46e5bf90357cda16398b5a0a5b883
GUSb <GUSbYa@gmail.com>
2014-06-16 12:40:52 +0300

    refactored and cleaned up controllers

--- src/main/java/com/softserveinc/tender/web/RoleController.java
+++ src/main/java/com/softserveinc/tender/web/RoleController.java
@@ -1,13 +1,9 @@
 package com.softserveinc.tender.web;
 
 import com.softserveinc.tender.dto.RoleDto;
-import com.softserveinc.tender.dto.UserRegistrationDataDto;
-import com.softserveinc.tender.entity.User;
 import com.softserveinc.tender.facade.RegistrationServiceFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,18 +11,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
-public class RegistrationController {
+@RequestMapping("/roles")
+public class RoleController {
 
     @Autowired
     private RegistrationServiceFacade registrationServiceFacade;
 
-    @RequestMapping(value = "/usersRoles", method = RequestMethod.GET)
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
     public @ResponseBody List<RoleDto> findUsersRoles() {
         return registrationServiceFacade.findUsersRoles();
-    }
-
-    @RequestMapping(value = "/userData", method = {RequestMethod.PUT, RequestMethod.GET}, consumes = "application/json")
-    public @ResponseBody User saveUserRegistrationData(@RequestBody UserRegistrationDataDto userData) {
-        return registrationServiceFacade.saveUser(userData);
     }
 }
