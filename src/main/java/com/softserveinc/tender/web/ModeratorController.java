9cb9e90c9aedcb05b75263a7676ba59ff9d5071a
rsolov <romansoloviy@gmail.com>
2014-06-26 13:36:21 -1000

    Fixed bug

--- src/main/java/com/softserveinc/tender/web/ModeratorController.java
+++ src/main/java/com/softserveinc/tender/web/ModeratorController.java
@@ -13,19 +13,5 @@ import java.util.List;
 @Controller
 @RequestMapping("/moderator")
 public class ModeratorController {
-    @Autowired
-    private DealServiceFacade dealFacade;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public @ResponseBody
-    List<DealDto> findDeals(@RequestParam(value = "pageNumber",required = true) Integer pageNumber,
-                            @RequestParam(value = "pageSize",required = true) Integer pageSize,
-                            @RequestParam(value = "orderBy", required = false, defaultValue = "date") String orderBy,
-                            @RequestParam(value = "sortDirection", required = false, defaultValue = "DESC") String sortDirection,
-                            @RequestParam(value = "searchParam",required = false) String searchParam) {
-
-        Sort.Direction pageSortDirection = Sort.Direction.fromString(sortDirection);
-
-        return dealFacade.findAllDeals(new PageRequest(pageNumber, pageSize, pageSortDirection, orderBy), searchParam);
-    }
 }
