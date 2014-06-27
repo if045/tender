161b0115cbf1d7e6b1c7e65bd5d3a1be8105bdeb
Khrystyna Makar <tinochka0@gmail.com>
2014-06-27 21:02:32 +0300

    Delete comment

--- src/main/webapp/WEB-INF/resources/js/header.js
+++ src/main/webapp/WEB-INF/resources/js/header.js
@@ -43,13 +43,6 @@ $(document).ready(function () {
             }
         });
     });
-
-    /*$.getJSON(PROPOSALS_URL + "?isNew=true&userRole=" + getCookie("userRole"), function(data){
-        var newProposalsNumber = data;
-        if(newProposalsNumber > 0) {
-            $("#my_tenders_btn").html("My tenders <span class='badge'>" + newProposalsNumber + "</span>");
-        }
-    });*/
 });
 
 function goToMyTenders() {
